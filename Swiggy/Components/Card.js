802b4f05b268e058d9da5649df8f8adc1c85a95b
Lokesh <lokeshsingh7695@gmail.com>
2025-01-01 20:00:06 +0530

    Added more styling

--- Swiggy/Components/Card.js
+++ Swiggy/Components/Card.js
@@ -5,8 +5,8 @@ const Card = (props) => {
       <div className="card">
         <img src={CDN_LINK +  cloudinaryImageId} alt="logo" className='card-img' />
         <h2>{name}</h2>
-        <p>{avgRating} .  {sla.slaString}</p>
-        <p>{cuisines}</p>
+        <p className="bold"><span className="star"><i class="fa-solid fa-star"></i></span> {avgRating} .  {sla.slaString}</p>
+        <p>{cuisines.join(", ")}</p>
         <p>{areaName}</p>
       </div>
     )
